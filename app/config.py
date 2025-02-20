ab9d601991cfd66af0777ae7f66c9182b9246701
tushar-3549 <tushardiu3549@gmail.com>
2025-02-20 22:08:29 +0600

    dockerfile added

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     database_password: str
     database_name: str
     database_username: str
-    secrect_key: str
+    secret_key: str
     algorithm: str
     access_token_expire_minutes: int 
 
